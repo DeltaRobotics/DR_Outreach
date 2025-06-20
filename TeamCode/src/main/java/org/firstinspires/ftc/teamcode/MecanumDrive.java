2b0851ed7ba599cd1a5fda687032686dfd0c2894
DeltaRobotics-FTC <deltaroboticsftc@gmail.com>
2025-06-19 20:55:50 -0500

    Made the acceleration smoother.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -4,17 +4,22 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(group = "TeleOps", name = "Mecanum Drive")
 public class MecanumDrive extends OpMode
 {
     private final double MAX_POWER = 0.5;
 
+    private boolean firstLoop = true;
+
+    private SmoothScaler ssHeading = new SmoothScaler(100, 50);
+    private SmoothScaler ssForward = new SmoothScaler(100, 50);
+    private SmoothScaler ssStrafe = new SmoothScaler(100, 50);
     private DcMotor motorLF, motorLB, motorRF, motorRB = null;
 
     private void MotorInit(DcMotor motor)
     {
-        motor.setDirection(DcMotorSimple.Direction.FORWARD);
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor.setPower(0);
     }
@@ -28,19 +33,39 @@ public class MecanumDrive extends OpMode
         motorRF = hardwareMap.get(DcMotor.class, "motorRF");
         motorRB = hardwareMap.get(DcMotor.class, "motorRB");
 
+        motorLB.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorLF.setDirection(DcMotorSimple.Direction.REVERSE);
+
         MotorInit(motorLF);
         MotorInit(motorLB);
         MotorInit(motorRF);
         MotorInit(motorRB);
+
+
+
     }
 
     @Override
     public void loop()
     {
-        motorLF.setPower(Math.min(gamepad1.right_stick_y, MAX_POWER));
-        motorLB.setPower(Math.min(gamepad1.right_stick_y, MAX_POWER));
-        motorRF.setPower(Math.min(gamepad1.right_stick_y, MAX_POWER));
-        motorRB.setPower(Math.min(gamepad1.right_stick_y, MAX_POWER));
+        if(firstLoop){
+            ssForward.elapsedTime.reset();
+            ssStrafe.elapsedTime.reset();
+            ssHeading.elapsedTime.reset();
+            firstLoop = false;
+        }
+
+        mecanumDrive( ssForward.smoothScaler(-gamepad1.right_stick_y), ssStrafe.smoothScaler(gamepad1.right_stick_x), ssHeading.smoothScaler(gamepad1.left_stick_x), MAX_POWER);
 
     }
+
+    public void mecanumDrive(double forward, double strafe, double heading, double speed){
+
+        motorRF.setPower((((forward - strafe) * 1) - (heading * 1)) * speed);
+        motorRB.setPower((((forward + strafe) * 1) - (heading * 1)) * speed);
+        motorLB.setPower((((forward - strafe) * 1) + (heading * 1)) * speed);
+        motorLF.setPower((((forward + strafe) * 1) + (heading * 1)) * speed);
+    }
+
+
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SmoothScaler.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class SmoothScaler {
+
+    private  int sampleRate;
+    //in Milliseconds
+    private  int sampleTimeDelta;
+    private  int sampleCount;
+
+    private int sampleIndex;
+
+    public ElapsedTime elapsedTime;
+
+    private double[] scalerSamples;
+
+    public SmoothScaler( int sampleRate, int sampleCount){
+
+        this.sampleRate = sampleRate;
+        this.sampleCount = sampleCount;
+        this.sampleTimeDelta = (int)(1 / (double) sampleRate * 1000);
+
+        sampleIndex = 0;
+        elapsedTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+        scalerSamples = new double[sampleCount];
+
+        for(int i = 0; i < scalerSamples.length; i++){
+
+            scalerSamples[i] = 0;
+
+        }
+    }
+    public double smoothScaler (double input){
+
+        if(elapsedTime.time() >= sampleTimeDelta){
+
+            scalerSamples[sampleIndex] = input;
+
+            elapsedTime.reset();
+
+            if(sampleIndex == (sampleCount - 1)){
+
+                sampleIndex = 0;
+            }
+            else{
+
+                sampleIndex ++;
+            }
+        }
+
+        double sampleSum = 0;
+
+        for(double sample : scalerSamples){
+
+            sampleSum += sample;
+        }
+
+        return sampleSum / sampleCount;
+    }
+}
